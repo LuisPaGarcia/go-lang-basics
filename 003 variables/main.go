327628a7f4877c2a4902ef1f6d5683bc8d381c89
LuisPa <diganluispa@gmail.com>
2019-08-31 23:12:19 -0600

    First commit

---
+++ 003 variables/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	var a = "inicial"
+	fmt.Println(a)
+	
+	var b, c int = 1, 2
+	fmt.Println(b,c)
+
+	var d=true
+	fmt.Println(d)
+
+	var i int 
+	i= 10
+	fmt.Println(i)
+
+	f := "apple"
+	fmt.Println(f)
+
+}
---
+++ 004 constants/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "math"
+
+const s string = "constant"
+
+func main(){
+	fmt.Println(s)
+
+	const a = 500000
+
+	const d = 3e20/a
+	fmt.Println(d)
+
+	fmt.Println(int64(d))
+	fmt.Println(math.Sin(d))
+}
